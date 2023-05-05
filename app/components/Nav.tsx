4399354e6e78541587a8a48659fac76d78468054
Hud Wahab <hudwahab@gmail.com>
2023-05-05 06:36:12 -0600

    rounded avater on the right, dashboard logged in

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,25 +6,28 @@ import Image from "next/image";
 
 export default function Nav({ user }: Session) {
   return (
-    <nav>
+    <nav className="flex justify-between items-center py-8">
       <h1>Styled</h1>
-      <ul>
-        <li>Products</li>
+      <ul className="flex items-center gap-12">
         {/* If the user is not signed in*/}
         {!user && (
-          <li>
+          <li className="bg-teal-600 text-white py-2 px-4 rounded-md">
             <button onClick={() => signIn()}>Sign in</button>
           </li>
         )}
         {user && (
-          <li>
-            <Image
-              src={user?.image as string}
-              alt={user.name as string}
-              width={48}
-              height={48}
-            />
-          </li>
+          <div>
+            <li>
+              <Image
+                src={user?.image as string}
+                alt={user.name as string}
+                width={48}
+                height={48}
+                className="rounded-full"
+              />
+            </li>{" "}
+            <li>Dashboard</li>
+          </div>
         )}
       </ul>
     </nav>

0055043668c39ae1fa183bf23fc8a66b8b3288d1
Hud Wahab <hudwahab@gmail.com>
2023-05-06 05:28:37 -0600

    account for null price

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,11 +1,13 @@
+import { ProductType } from "@/types/ProductType";
+import formatPrice from "@/util/PriceFormat";
 import Image from "next/image";
 
-export default function Product({ name, image, price }) {
+export default function Product({ name, image, unit_amount }: ProductType) {
   return (
     <div>
       <Image src={image} alt={name} width={400} height={400} />
       <h1>{name}</h1>
-      {price}
+      {unit_amount !== null ? formatPrice(unit_amount) : "N/A"}
     </div>
   );
 }

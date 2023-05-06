6ed5b8b4eb6d7983ed5a39d8ad3cb63310245848
Hud Wahab <hudwahab@gmail.com>
2023-05-06 05:46:09 -0600

    add grid, formats

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -4,10 +4,20 @@ import Image from "next/image";
 
 export default function Product({ name, image, unit_amount }: ProductType) {
   return (
-    <div>
-      <Image src={image} alt={name} width={400} height={400} />
-      <h1>{name}</h1>
-      {unit_amount !== null ? formatPrice(unit_amount) : "N/A"}
+    <div className="text-gray-700 py-2">
+      <Image
+        src={image}
+        alt={name}
+        width={800}
+        height={800}
+        className="w-full h-80 object-cover rounded-lg"
+      />
+      <div className="font-medium">
+        <h1>{name}</h1>
+        <h2 className="text-sm text-teal-700">
+          {unit_amount !== null ? formatPrice(unit_amount) : "N/A"}{" "}
+        </h2>
+      </div>
     </div>
   );
 }

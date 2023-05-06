09e062d9c262a7887a4c50613c87101b83d7ee2a
Hud Wahab <hudwahab@gmail.com>
2023-05-06 04:53:59 -0600

    get products

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
-export default function Home() {
+import Stripe from "stripe";
+
+const getProducts = async () => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+    apiVersion: "2022-11-15",
+  });
+  const products = await stripe.products.list();
+};
+
+export default async function Home() {
+  const products = await getProducts();
   return (
     <main>
       <h1>Welcome to the beginning, where time did not exist.</h1>
